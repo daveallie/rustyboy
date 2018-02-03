5da052b577d0799b470663fcca214ca56a0106f2
Dave Allie <dave@daveallie.com>
2018-02-03 20:59:45 +1000

    Add debugger with some basic commands

---
+++ src/debugger.rs
@@ -0,0 +1,194 @@
+use std::process;
+use std::io::{self, Write};
+use cpu;
+use register::Flags;
+
+pub struct Debugger {
+    current_cycles: u32,
+    debugging: bool,
+    debug_after_cycles_enabled: bool,
+    debug_after_cycles: u32,
+    cpu: cpu::CPU,
+    reg_break_points: Vec<RegBreakPoint>,
+}
+
+struct RegBreakPoint {
+    key: String,
+    value: u32,
+}
+
+impl Debugger {
+    pub fn new(debug_after_cycles: Option<u32>, cpu: cpu::CPU) -> Debugger {
+        Debugger {
+            current_cycles: 0,
+            debugging: false,
+            debug_after_cycles_enabled: debug_after_cycles.is_some(),
+            debug_after_cycles: debug_after_cycles.unwrap_or(0),
+            cpu,
+            reg_break_points: vec![],
+        }
+    }
+
+    pub fn run(&mut self) {
+        loop {
+            self.current_cycles += self.cpu.step();
+            if self.should_stop() {
+                self.debug();
+            }
+        }
+    }
+
+    fn should_stop(&self) -> bool {
+        let register = self.cpu.reg;
+        let break_points = &self.reg_break_points;
+
+        for break_point in break_points {
+            let key = break_point.key.as_str();
+            let reg_value = match key {
+                "a" => register.a as u16,
+                "b" => register.b as u16,
+                "c" => register.c as u16,
+                "d" => register.d as u16,
+                "e" => register.e as u16,
+                "h" => register.h as u16,
+                "l" => register.l as u16,
+                "pc" => register.pc,
+                "sp" => register.sp,
+                _ => panic!("Unknown register {}\n", key),
+            } as u32;
+
+            if reg_value == break_point.value {
+                output(&format!("Breaking as {} is {}\n", key, reg_value));
+                return true
+            }
+        }
+
+        self.debug_after_cycles_enabled && self.current_cycles >= self.debug_after_cycles
+    }
+
+    fn debug(&mut self) {
+        self.debugging = true;
+        while self.debugging {
+            self.read_input_and_process()
+        }
+    }
+
+    fn read_input_and_process(&mut self) {
+        let line = read_line();
+        let mut words = line.trim().split(" ");
+        match words.next() {
+            Some("c") | Some("continue") => {
+                self.debug_after_cycles_enabled = false;
+                self.debugging = false;
+            },
+            Some("n") | Some("next") => {
+                let jump = read_num(words.next().unwrap_or("0"));
+                self.debug_after_cycles = self.current_cycles + jump;
+                self.debugging = false;
+            },
+            Some("bo") | Some("breakon") => {
+                match words.next().unwrap() {
+                    "register" => {
+                        let key = words.next().unwrap().to_owned();
+                        let value = read_num(words.next().unwrap_or("0"));
+                        self.reg_break_points.push(RegBreakPoint {
+                            key,
+                            value,
+                        });
+                        self.debug_after_cycles_enabled = false;
+                        self.debugging = false;
+                    }
+                    _ => output("Don't know what you mean\n")
+                }
+            },
+            Some("reg") | Some("registers") => {
+                let action = words.next();
+
+                if action.is_some() && action.unwrap() == "write" {
+                    let reg_key = words.next().unwrap();
+                    let reg_value = read_num(words.next().unwrap());
+
+                    match reg_key {
+                        "a" => self.cpu.reg.a = reg_value as u8,
+                        "b" => self.cpu.reg.b = reg_value as u8,
+                        "c" => self.cpu.reg.c = reg_value as u8,
+                        "d" => self.cpu.reg.d = reg_value as u8,
+                        "e" => self.cpu.reg.e = reg_value as u8,
+                        "h" => self.cpu.reg.h = reg_value as u8,
+                        "l" => self.cpu.reg.l = reg_value as u8,
+                        "pc" => self.cpu.reg.pc = reg_value as u16,
+                        "sp" => self.cpu.reg.sp = reg_value as u16,
+                        _ => panic!("Unknown register {}\n", reg_key),
+                    };
+                } else {
+                    let register = self.cpu.reg;
+                    output("8 bit registers:\n");
+                    output(&format!("a: 0x{:X}\n", register.a));
+                    output(&format!("b: 0x{:X}\n", register.b));
+                    output(&format!("c: 0x{:X}\n", register.c));
+                    output(&format!("d: 0x{:X}\n", register.d));
+                    output(&format!("e: 0x{:X}\n", register.e));
+                    output(&format!("h: 0x{:X}\n", register.h));
+                    output(&format!("l: 0x{:X}\n", register.l));
+
+                    output("\n16 bit registers:\n");
+                    output(&format!("pc: 0x{:X}\n", register.pc));
+                    output(&format!("sp: 0x{:X}\n", register.sp));
+
+                    output("\nFlags:\n");
+                    output(&format!("Z: {}\n", register.get_flag(Flags::Z)));
+                    output(&format!("N: {}\n", register.get_flag(Flags::N)));
+                    output(&format!("H: {}\n", register.get_flag(Flags::H)));
+                    output(&format!("C: {}\n", register.get_flag(Flags::C)));
+                }
+            },
+            Some("lastbyte") => {
+                output(&format!("0x{:X}\n", self.cpu.mmu.read_byte(self.cpu.reg.pc - 1)));
+            },
+            Some("rb") => {
+                let addr = read_num(words.next().unwrap_or("0")) as u16;
+                output(&format!("0x{:X}\n", self.cpu.mmu.read_byte(addr)));
+            },
+            Some("rw") => {
+                let addr = read_num(words.next().unwrap_or("0")) as u16;
+                output(&format!("0x{:X}\n", self.cpu.mmu.read_word(addr)));
+            },
+            Some("dump") => self.dump(words.next().unwrap_or("mem_dump")),
+            Some("exit") => process::exit(1),
+            _ => output("Unknown command!\n")
+        };
+    }
+
+    fn dump(&self, folder_name: &str) {
+        // wram
+        // self.cpu.dump_wram();
+
+        // zram
+        // self.cpu.dump_zram();
+
+        // oam
+        // self.cpu.dump_oam(folder_name + "/oam.dmp");
+    }
+}
+
+fn read_num(num_str: &str) -> u32 {
+    if num_str.starts_with("0x") {
+        u32::from_str_radix(&num_str[2..], 16).unwrap()
+    } else {
+        num_str.parse::<u32>().unwrap()
+    }
+}
+
+fn read_line() -> String {
+    output("What do you want?\n");
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    buffer
+}
+
+fn output(line: &str) {
+    let stdout = io::stdout();
+    let mut handle = stdout.lock();
+    handle.write(line.as_bytes()).unwrap();
+    handle.flush();
+}
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,26 @@ mod gpu;
 mod mmu;
 mod register;
 mod serial;
+#[cfg(feature = "debugger")]
+mod debugger;
 
 fn main() {
     let cart_path = env::args().nth(1).unwrap();
     let mut cpu = cpu::CPU::new(&cart_path);
 
+    run(cpu);
+}
+
+#[cfg(not(feature = "debugger"))]
+fn run(mut cpu: cpu::CPU) {
     loop {
         cpu.step();
     }
+}
+
+#[cfg(feature = "debugger")]
+fn run(mut cpu: cpu::CPU) {
+    let debug_after_cycles = env::args().nth(2).map(|item| item.parse::<u32>().unwrap());
+    let mut debugger = debugger::Debugger::new(debug_after_cycles, cpu);
+    debugger.run();
 }
