4b14b1bc49d2fc0d8579ae770de4afb849c1d3bc
Dave Allie <dave@daveallie.com>
2018-02-03 20:10:19 +1000

    Add read/write support for GPU palette data

--- src/gpu.rs
+++ src/gpu.rs
@@ -2,6 +2,9 @@ const VIDEO_RAM_SIZE: usize = 0x2000;
 
 pub struct GPU {
     video_ram: [u8; VIDEO_RAM_SIZE],
+    bg_palette: u8,
+    obj_palette_0: u8,
+    obj_palette_1: u8,
     oam: [u8; 160], // Sprite attribute table
     lcd_control: u8,
     stat: u8,
@@ -14,6 +17,9 @@ impl GPU {
     pub fn new() -> GPU {
         GPU {
             video_ram: [0u8; VIDEO_RAM_SIZE],
+            bg_palette: 0,
+            obj_palette_0: 0,
+            obj_palette_1: 0,
             oam: [0u8; 160],
             lcd_control: 0x91,
             stat: 0,
@@ -42,6 +48,9 @@ impl GPU {
             0xFF42 => self.scy,
             0xFF43 => self.scx,
             0xFF44 => self.ly,
+            0xFF47 => self.bg_palette,
+            0xFF48 => self.obj_palette_0,
+            0xFF49 => self.obj_palette_1,
             _ => panic!("Unknown GPU control read operation: 0x{:X}", addr),
         }
     }
@@ -53,6 +62,9 @@ impl GPU {
             0xFF42 => self.scy = value,
             0xFF43 => self.scx = value,
             0xFF44 => (), // read only
+            0xFF47 => self.bg_palette = value,
+            0xFF48 => self.obj_palette_0 = value,
+            0xFF49 => self.obj_palette_1 = value,
             _ => panic!("Unknown GPU control write operation: 0x{:X}", addr),
         }
     }
